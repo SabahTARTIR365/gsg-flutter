51c7bfcd17f92478cd465d67cd562cb16399e3b8
SabahTARTIR365 <sosotarteer2017@gmail.com>
2022-10-12 17:57:36 +0300

    add tab controller for the code

--- lib/task_managementt_app/main_task_screen.dart
+++ lib/task_managementt_app/main_task_screen.dart
@@ -12,7 +12,7 @@ class Main_Task extends StatefulWidget{
   State<Main_Task> createState() => _Main_TaskState();
 }
 
-class _Main_TaskState extends State<Main_Task> {
+class _Main_TaskState extends State<Main_Task> with SingleTickerProviderStateMixin{
   checkTask(TaskModel taskModel){
     int index =tasks.indexOf(taskModel);
     tasks[index].isComplete=!tasks[index].isComplete;
@@ -21,69 +21,89 @@ class _Main_TaskState extends State<Main_Task> {
     });
 
   }
-
+  late TabController tabController;
+  inisializeTabController(){
+    tabController=TabController(length: 3, vsync: this);
+  }
+@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    inisializeTabController();
+  }
   @override
   Widget build(BuildContext context) {
-return DefaultTabController(
-  length: 3,
-  child:   Scaffold(
-    drawer: Drawer(child: Column(
-      children: [
-      UserAccountsDrawerHeader(
-        currentAccountPicture: CircleAvatar(child: Text('ST'),),
-          accountName: Text('Sabah tartir'),
-          accountEmail: Text('Sabahtartir99@gmail.com')),
+return Scaffold(
+  drawer: Drawer(child: Column(
+    children: [
+    UserAccountsDrawerHeader(
+      currentAccountPicture: CircleAvatar(child: Text('ST'),),
+        accountName: Text('Sabah tartir'),
+        accountEmail: Text('Sabahtartir99@gmail.com')),
 
-        ListTile(
-          title: Text('All tasks'),
-          subtitle: Text('Go to show All tasks'),
-          leading:Icon(Icons.view_headline_sharp),
+      ListTile(
+        onTap: (){
+          tabController.animateTo(0);
+        },
+        title: Text('All tasks'),
+        subtitle: Text('Go to show All tasks'),
+        leading:Icon(Icons.view_headline_sharp),
+      trailing: Icon(Icons.arrow_forward_ios),),
+
+      ListTile(
+        onTap: (){
+          tabController.animateTo(1);
+        },
+        title: Text('Completed tasks'),
+        subtitle: Text('Go to show the completed tasks'),
+        leading:Icon(Icons.done),
         trailing: Icon(Icons.arrow_forward_ios),),
 
-        ListTile(
-          title: Text('Completed tasks'),
-          subtitle: Text('Go to show the completed tasks'),
-          leading:Icon(Icons.done),
-          trailing: Icon(Icons.arrow_forward_ios),),
+      ListTile(
+        onTap: (){
+          tabController.animateTo(2);
+          
+        },
+        title: Text('InCompleted tasks'),
+        subtitle: Text('Go to show the incompleted tasks'),
+        leading:Icon(Icons.clear),
+        trailing: Icon(Icons.arrow_forward_ios),),
+  ],
+  ),
+  ),
 
-        ListTile(
-          title: Text('InCompleted tasks'),
-          subtitle: Text('Go to show the incompleted tasks'),
-          leading:Icon(Icons.clear),
-          trailing: Icon(Icons.arrow_forward_ios),),
-    ],
-    ),
-    ),
+  appBar: AppBar(
 
-    appBar: AppBar(
+    title: const Text('Tasks App'),
 
-      title: const Text('Tasks App'),
+    bottom: TabBar(
+      controller: tabController,
+      tabs: [
 
-      bottom: TabBar(tabs: [
+      Tab(text: 'All tasks',),
 
-        Tab(text: 'All tasks',),
+      Tab(text: 'Done tasks',),
 
-        Tab(text: 'Done tasks',),
-
-        Tab(text: 'Uncompleted tasks',),
+      Tab(text: 'Uncompleted tasks',),
 
 
 
-      ],),
+    ],),
 
-    ),
+  ),
 
-body: TabBarView(children:[
+body: TabBarView(
+    controller: tabController,
+    children:[
  AllTasksScreen( checkTask),
-  CompleteTasksScreen( checkTask),
-  IncompleteTasksScreen( checkTask),
+CompleteTasksScreen( checkTask),
+IncompleteTasksScreen( checkTask),
 
 
 ]),
 
 
 
-  ),
 );
   }
 }
